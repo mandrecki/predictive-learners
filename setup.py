5ba6b8ec8a24f50c164399bbbe1e6645732f4178
Marian Andrecki <38013008+EqThinker@users.noreply.github.com>
2018-07-31 23:38:27 +0100

    Initial files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DeepTrack',
+    version='0.1dev',
+    packages=['deep_track',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
