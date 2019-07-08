ade394e76cd31df2fd6cf4e77ab7fd5f958393f8
Marian Andrecki <marian.andrecki+eqt@gmail.com>
2019-07-08 08:37:41 +0100

    added planet model draft

--- pred_learn/models/__init__.py
+++ pred_learn/models/__init__.py
@@ -1,4 +1,4 @@
-from .rnn import PredictorRNN
-from .vae_wm import VAE
-from .conv_tranistion_i2a import RecursiveConvTransition
-from .planet_model import TransitionModel, VisualEncoder, VisualObservationModel
+# from .rnn import PredictorRNN
+# from .vae_wm import VAE
+# from .conv_tranistion_i2a import RecursiveConvTransition
+# from .planet_model import TransitionModel, VisualEncoder, VisualObservationModel
